2f31771b74ac0744523853803c1328dd56ae3f9e
Niranjan Maharana <niranjan.maharana@signify.com>
2021-12-01 19:18:12 +0530

    added entity, repo and controller for testing

---
+++ src/test/java/com/niranzan/jpa/SpringBootJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niranzan.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
